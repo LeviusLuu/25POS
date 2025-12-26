ae56b322aeece9e3c71feb1b37abafb5d69a829d
LeviusLuu <163327462+LeviusLuu@users.noreply.github.com>
2025-12-26 04:49:52 +0000

    save

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { TestModule } from './test/test.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { ConfigModule } from '@nestjs/config';
       autoLoadEntities: true,
       synchronize: true, // ⚠️ chỉ dùng dev
     }),
+    TestModule,
   ],
   controllers: [AppController],
   providers: [AppService],
